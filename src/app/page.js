367624b7e470a3278ec46350814953daa0fd4eb4
janet-ulfiya <janetulfiya@dmice.ac.in>
2026-06-10 07:08:18 +0000

    Remove main wrapper

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,5 @@
-import Chatbot from '@/components/Chatbot';
+import Chatbot from "@/components/Chatbot";
 
 export default function Home() {
-  return (
-    <main className="main-content">
-      <Chatbot />
-      
-      <style>{`
-        .main-content {
-          min-height: 100vh;
-          position: relative;
-          z-index: 1;
-        }
-      `}</style>
-    </main>
-  );
+  return <Chatbot />;
 }
